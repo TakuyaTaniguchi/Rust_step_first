0fcc7dd10dd2b505b97cb3b69a4f6bb7a6831fdf
ttaniguchi <ttaniguchi@oikaze.jp>
2019-05-26 08:02:43 +0900

    Update Hello world

---
+++ hello/target/debug/deps/hello-cebef50220cda3ad.d
@@ -0,0 +1,5 @@
+/Users/oikaze/work/sandbox/rust_step_first/hello/target/debug/deps/hello-cebef50220cda3ad: src/main.rs
+
+/Users/oikaze/work/sandbox/rust_step_first/hello/target/debug/deps/hello-cebef50220cda3ad.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/oikaze/work/sandbox/rust_step_first/hello/target/debug/hello.dSYM: /Users/oikaze/work/sandbox/rust_step_first/hello/src/main.rs
